58f80c3cbe9ffa765b47be745797efca80e269a9
Ramses Martinez <ramses.mtz96@gmail.com>
2016-06-14 20:29:53 -0500

    Terminado estilo login, y conexion a facebook. Creada base para ShakeActivity

--- app/src/main/java/mx/ramsesmartinez/shakeapp/MainActivity.java
+++ app/src/main/java/mx/ramsesmartinez/shakeapp/MainActivity.java
@@ -1,8 +1,13 @@
 package mx.ramsesmartinez.shakeapp;
 
+import android.annotation.TargetApi;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
@@ -14,16 +19,29 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnFocusChangeListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.squareup.picasso.Picasso;
+
 public class MainActivity extends AppCompatActivity implements OnClickListener, OnFocusChangeListener {
 
-    Bitmap originalBitmap;
-    Drawable originalDrawable;
+    Bitmap bitmapImageProfile;
+    CallbackManager callbackManager;
+    Drawable drawableImageProfile;
     RoundedBitmapDrawable roundedDrawable;
 
     EditText editTextEmail;
@@ -33,20 +51,45 @@ public class MainActivity extends AppCompatActivity implements OnClickListener, 
     TextView textViewPassword;
 
 
+    public void setToolbar(){
+        Toolbar toolbar =(Toolbar) findViewById(R.id.activity_my_toolbar);
+        toolbar.setTitle("Login");
+        setSupportActionBar(toolbar);
+    }
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
         setToolbar();
 
-        //Extracts the drawable in a bitmap
-        originalDrawable = getResources().getDrawable(R.drawable.img_boy);
-        originalBitmap = ((BitmapDrawable) originalDrawable).getBitmap();
+        callbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.button_login_facebook);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+        drawableImageProfile= getResources().getDrawable(R.drawable.img_boy);
+        bitmapImageProfile = ((BitmapDrawable) drawableImageProfile).getBitmap();
 
         //Creates the rounded drawable
-        roundedDrawable = RoundedBitmapDrawableFactory.create(getResources(), originalBitmap);
-        roundedDrawable.setCornerRadius(originalBitmap.getHeight());
+        roundedDrawable = RoundedBitmapDrawableFactory.create(getResources(), bitmapImageProfile);
+        roundedDrawable.setCornerRadius(bitmapImageProfile.getHeight());
 
         editTextEmail= (EditText) findViewById(R.id.edit_text_email);
         editTextPassword = (EditText) findViewById(R.id.edit_text_password);
@@ -57,15 +100,13 @@ public class MainActivity extends AppCompatActivity implements OnClickListener, 
         editTextEmail.setOnFocusChangeListener(this);
         editTextPassword.setOnFocusChangeListener(this);
         imageViewProfileLogin.setImageDrawable(roundedDrawable);
-
     }
 
-    public void setToolbar(){
-        Toolbar toolbar =(Toolbar) findViewById(R.id.activity_my_toolbar);
-        toolbar.setTitle("Login");
-        setSupportActionBar(toolbar);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
-
 
     @Override
     public void onClick(View v) {
@@ -113,4 +154,5 @@ public class MainActivity extends AppCompatActivity implements OnClickListener, 
                 break;
         }
     }
+
 }
